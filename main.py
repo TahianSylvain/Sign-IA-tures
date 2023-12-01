f781f20f71ff0b5fb90d031c6bbc82e0939af4d2
tahiana <TahianSylvain>
2023-12-01 14:49:58 +0300

    All is working but not very well though

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.platypus import SimpleDocTemplate, Paragraph\
+    , Image, SimpleDocTemplate
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib.units import inch
+
+""" New to download the Times-New-Roman.ttf
+from reportlab.lib.fonts import ttfonts
+from reportlab.pdfbase import pdfmetrics
+
+pdfmetrics.registerFont(ttfonts.TTFont(
+    'Times-New-Roman', 'Times-New-Roman.ttf'))
+"""
 
 
 def generate_pdf(text):
@@ -9,14 +18,19 @@ def generate_pdf(text):
 
     # Create a paragraph style with Times New Roman font
     style = ParagraphStyle(name='TimesNewRoman',
-                           fontName='Times-New-Roman',
+                        #    fontName='Times-New-Roman',
                            fontSize=12)
+
+    #Create an Img from the media/file
+    image: any = object
+    # image = Image('./lvlind10.png', x=200, y=500, width=200, height=200)
 
     # Create a paragraph with the text
     paragraph = Paragraph(text, style)
 
     # Add the paragraph to the document
-    doc.build([paragraph])
+    doc.build([paragraph, image])
+
 
 if __name__ == '__main__':
     text = 'This is a test string in Times New Roman font.'
