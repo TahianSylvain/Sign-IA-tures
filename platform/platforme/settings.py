fbfc07df13d40e34b7f67147b8243b03c306f290
Sylvain <124794207+TahianSylvain@users.noreply.github.com>
2024-03-27 08:06:05 +0300

    Update settings.py
    Signed-off-by: Sylvain <124794207+TahianSylvain@users.noreply.github.com>

--- platform/platforme/settings.py
+++ platform/platforme/settings.py
@@ -110,10 +110,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
