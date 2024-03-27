733ae877981ff98ba0fb365e276e38e6f6cca9a6
Sylvain <124794207+TahianSylvain@users.noreply.github.com>
2024-03-27 08:05:33 +0300

    Update settings.py
    Signed-off-by: Sylvain <124794207+TahianSylvain@users.noreply.github.com>

--- platform/platforme/settings.py
+++ platform/platforme/settings.py
@@ -61,10 +61,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'platforme.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
